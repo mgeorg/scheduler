14e86763f8449395fd3e550f8956642deecfdca2
Manfred Georg <manfred.georg@gmail.com>
2014-09-23 09:17:56 -0700

    Moved remaining code into functions, doesn't work together.

--- sched.py
+++ sched.py
@@ -15,6 +15,7 @@ import csv
 import subprocess
 import tempfile
 
+
 class Scheduler:
   def __init__(self):
     self.next_var = 1
@@ -25,6 +26,7 @@ class Scheduler:
     self.reverse_variables = dict()
     self.constraints = []
     self.num_students = 0
+
 
   def ParseTimeRow(self, row):
     assert row[0] == 'Schedule', 'The first row must start with \'Schedule\'.'
@@ -37,6 +39,7 @@ class Scheduler:
       self.slot_day[i] = day
       self.day_slot[day].append(i)
       self.slot_time[i] = time
+
 
   def ParseFile(self, file_name):
     with open(file_name, 'rb') as csvfile:
@@ -68,6 +71,7 @@ class Scheduler:
               self.constraints.append('1 '+x_name+' = 0;')
           self.num_students += 1
 
+
   # TODO(mgeorg) Make this function set all the constraints based on local variables.  Hold full data for each slot and each student within the class.
   def MakeConstraints(self):
     # Each student only has 1 class.
@@ -90,66 +94,83 @@ class Scheduler:
       self.constraints.append('1 ' + variables['t'+str(t)] + ' -1 ' +
                               ' -1 '.join(x_names) + ' = 0;')
 
+
+  def MakeObjective(self):
+    all_products = dict()
+    objective = []
+    vars = []
+    for t in xrange(len(sched_row)-1):
+      var_name = 't'+str(t)
+      x_name = variables[var_name]
+      vars.append(x_name)
+      vars.sort()
+      product = '~' + ' ~'.join(vars)
+      all_products[product] = 1
+      objective.append(' ' + str(-100-10*(t)) + ' ' + product)
+
+    vars = []
+    for t in xrange(len(sched_row)-1):
+      var_name = 't'+str(len(sched_row)-2-t)
+      x_name = variables[var_name]
+      vars.append(x_name)
+      vars.sort()
+      product = '~' + ' ~'.join(vars)
+      all_products[product] = 1
+      objective.append(' ' + str(-150-15*(t)) + ' ' + product)
+
+
+  def MakeHeader(self):
+    self.header = (
+        '* #variable= '+str(max_var-1)+' #constraint= '+str(len(constraints))+
+        ' #product= '+str(len(all_products))+
+        ' sizeproduct= '+str(len(sched_row)-1))
+    
+
+  def WriteFile(self):
+    (handle_int, self.opb_file) = tempfile.mkstemp()
+    print self.opb_file
+    handle = os.fdopen(handle_int, 'w')
+    handle.write(self.header + '\n')
+    handle.write('min: ' + ''.join(objective) + ';\n')
+    handle.write('\n'.join(constraints) + '\n')
+    handle.close()
+
+
+  def RunSolver(self):
+    p = subprocess.Popen(['clasp', file_name], stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    (self.solver_output, unused_stderr) = p.communicate()
+    print self.solver_output
+
+
+  def ParseSolverOutput(self):
+    vars = []
+    for line in stdout.splitlines():
+      m = re.match('v(?:\s+-?x\d+)*', line)
+      if m:
+        curr_vars = line.split(' ')
+        assert curr_vars[0] == 'v'
+        vars.extend(curr_vars[1:])
+  
+    # print ' '.join(vars)
+
+    xs = []
+    for v in vars:
+      m = re.match('(-)?(x\d+)', v)
+      assert m
+      if not m.group(1):
+        x = reverse_variables[m.group(2)]
+        xs.append(x)
+
+    print ' '.join(xs)
+
+
 s = Scheduler()
 s.ParseFile(r'sched.csv')
 s.MakeConstraints()
-
-all_products = dict()
-objective = []
-vars = []
-for t in xrange(len(sched_row)-1):
-  var_name = 't'+str(t)
-  x_name = variables[var_name]
-  vars.append(x_name)
-  vars.sort()
-  product = '~' + ' ~'.join(vars)
-  all_products[product] = 1
-  objective.append(' ' + str(-100-10*(t)) + ' ' + product)
-
-vars = []
-for t in xrange(len(sched_row)-1):
-  var_name = 't'+str(len(sched_row)-2-t)
-  x_name = variables[var_name]
-  vars.append(x_name)
-  vars.sort()
-  product = '~' + ' ~'.join(vars)
-  all_products[product] = 1
-  objective.append(' ' + str(-150-15*(t)) + ' ' + product)
-
-
-(handle_int, file_name) = tempfile.mkstemp()
-print file_name
-handle = os.fdopen(handle_int, 'w')
-handle.write(
-    '* #variable= '+str(max_var-1)+' #constraint= '+str(len(constraints))+
-    ' #product= '+str(len(all_products))+
-    ' sizeproduct= '+str(len(sched_row)-1)+'\n')
-handle.write('min: ' + ''.join(objective) + ';\n')
-handle.write('\n'.join(constraints) + '\n')
-handle.close()
-
-p = subprocess.Popen(['clasp', file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-(stdout, unused_stderr) = p.communicate()
-
-print stdout
-
-vars = []
-for line in stdout.splitlines():
-  m = re.match('v(?:\s+-?x\d+)*', line)
-  if m:
-    curr_vars = line.split(' ')
-    assert curr_vars[0] == 'v'
-    vars.extend(curr_vars[1:])
-  
-# print ' '.join(vars)
-
-xs = []
-for v in vars:
-  m = re.match('(-)?(x\d+)', v)
-  assert m
-  if not m.group(1):
-    x = reverse_variables[m.group(2)]
-    xs.append(x)
-
-print ' '.join(xs)
+s.MakeObjective()
+s.MakeHeader()
+s.WriteFile()
+s.RunSolver()
+s.ParseSolverOutput()
 
