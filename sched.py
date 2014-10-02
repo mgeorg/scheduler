af11df5b2cb36072e7ae3df79bc98a5f8a3cfcbb
Manfred Georg <manfred.georg@gmail.com>
2014-10-02 01:28:17 -0700

    Added a class for Constraints and started documentation.

--- sched.py
+++ sched.py
@@ -16,29 +16,18 @@ import subprocess
 import tempfile
 
 
-class Scheduler:
+class Constraints:
   def __init__(self):
-    self.next_var = 1
-    self.slot_day = []
-    self.slot_time = []
-    self.day_slot = dict()
-    self.variables = dict()
-    self.reverse_variables = dict()
-    self.constraints = []
-    self.num_students = 0
-
-
-  def ParseTimeRow(self, row):
-    assert row[0] == 'Schedule', 'The first row must start with \'Schedule\'.'
-    for i in xrange(len(row)-1):
-      slot = row[i+1]
-      m = re.match(r'(\S+)\s+(\d+):(\d+)', slot)
-      assert m, 'Slot did not match pattern'
-      day = m.group(1)
-      time = m.group(2)*60+m.group(3)
-      self.slot_day[i] = day
-      self.day_slot[day].append(i)
-      self.slot_time[i] = time
+    self.num_slots = 0
+    self.slot_name = []
+    self.pupil_name = []
+    self.slot_time = []  # Tuples of (day #, minute in day).
+    self.pupil_slot_preference = []  # pupil x slot with value 1,2,3.
+                                     # pupil zero is the instructor.
+    self.pupil_num_lessons = []  # The number of lessons this pupil has.
+    self.pupil_lesson_length = []  # The length of the lesson in minutes.
+    self.day_to_number = {'M' : 0, 'T' : 1, 'W' : 2, 'R' : 3,
+                          'F' : 4, 'S' : 5, 'U' : 6}
 
 
   def ParseFile(self, file_name):
@@ -48,17 +37,11 @@ class Scheduler:
         if row[0] == 'Schedule':
           self.ParseTimeRow(row)
         elif row[0] == 'Available':
-          assert len(self.slot_day), 'The first row must start with \'Schedule\''
-          assert len(row)-1 == len(self.slot_day), 'All rows must be of the same length.'
-          for i in xrange(len(row)-1):
-            var_name = 't'+str(i)
-            x_name = 'x'+str(self.next_var)
-            self.variables[var_name] = x_name
-            self.reverse_variables[x_name] = var_name
-            self.next_var += 1
-            if row[i+1] != 'y':
-              self.constraints.append('1 '+x_name+' = 0;')
+	  self.ParseAvailableRow(row)
         else:
+	  # TODO(mgeorg) Add row for Lunch constraints.
+	  # TODO(mgeorg) Add a row for complicated constraints.
+	  self.ParsePupilRow(row)
           assert len(self.slot_day), 'The first two rows were not formatted correctly.'
           assert len(row)-1 == len(self.slot_day), 'All rows must be of the same length.'
           for i in xrange(len(row)-1):
@@ -71,6 +54,56 @@ class Scheduler:
               self.constraints.append('1 '+x_name+' = 0;')
           self.num_students += 1
 
+  def ParseTimeRow(self, row):
+    assert row[0] == 'Schedule', 'The first row must start with \'Schedule\'.'
+    self.num_slots = len(row) - 1
+    self.slot_name = ["INVALID"] * num_slots
+    self.slot_time = [(0, 0)] * num_slots
+    for i in xrange(self.num_slots):
+      slot_name = row[i+1]
+      self.slot_name[i] = slot_name
+      m = re.match(r'([MTWRFSU])\s+(\d+):(\d+)', slot_name)
+      assert m, 'Slot "%s" did not match required pattern' % slot_name
+      day = self.day_to_number[m.group(1)], 
+      time = int(m.group(2))*60+int(m.group(3))
+      assert int(m.group(2)) >= 0, 'Specified hours was invalid.'
+      assert int(m.group(2)) < 24, 'Specified hours was invalid.'
+      assert int(m.group(3)) >= 0, 'Specified minutes was invalid.'
+      assert int(m.group(3)) < 60, 'Specified minutes was invalid.'
+      assert time >= 0, 'Specified time was negative.'
+      assert time < 24*60, 'Specified time was larger than a day'
+      # TODO(mgeorg) check non-decreasing day and increasing time.
+      self.slot_time[i] = (day, time)
+
+  def ParseAvailableRow(self, row):
+    assert row[0] == 'Available', 'The second row must start with \'Available\'.'
+    assert self.num_slots > 0, 'The first row was malformed.'
+    assert len(row)-1 == self.num_slots, 'All rows must be of the same length.'
+    assert len(self.pupil_slot_preference) == 0, 'The second row must be the instructors preferences.'
+
+    self.pupil_slot_preferences.add([0] * self.num_slots)
+    for i in xrange(self.num_slots):
+      if row[i+1] in ["1", "2", "3"]:
+        self.pupil_slot_preferences[0][i] = int(row[i+1])
+
+
+class Scheduler:
+  def __init__(self):
+    self.next_var = 1
+    self.slot_day = []
+    self.slot_time = []
+    self.day_slot = dict()
+    self.variables = dict()
+    self.reverse_variables = dict()
+    self.constraints = []
+    self.num_students = 0
+
+
+  def MakeVariable(self, var_name):
+    x_name = 'x'+str(self.next_var)
+    self.x_name[var_name] = x_name
+    self.our_name[x_name] = var_name
+    self.next_var += 1
 
   # TODO(mgeorg) Make this function set all the constraints based on local variables.  Hold full data for each slot and each student within the class.
   def MakeConstraints(self):
