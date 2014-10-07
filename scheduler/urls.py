00a1af744ee8ae0b86cb66023379183cc407a5e1
Manfred Georg <manfred.georg@gmail.com>
2014-10-07 05:48:41 +0000

    Commented out failing app code.

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'scheduler.views.home', name='home'),
+    #url(r'^$', home, name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+#def home(request, name='blah'):
+#  pass
 
