9bd6e7edff855d7cb7207fc644520e0dd626f928
Manfred Georg <manfred.georg@gmail.com>
2014-10-08 03:18:58 +0000

    Moved models to correct location.

--- solver/models.py
+++ solver/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Constraints:
+  creation_time = forms.DateTimeField()
+  deleted = models.BooleanField()
+  csv_table_data = forms.CharField(widget=forms.Textarea)
+
+class Schedule:
+  creation_time = forms.DateTimeField()
+  deleted = models.BooleanField()
+  score = models.IntegerField()
+  schedule = models.BinaryField()
+  created_by = models.ForeignKey(SolverRun)
+
+class SolverRun:
+  creation_time = forms.DateTimeField()
+  deleted = models.BooleanField()
+  options = models.ForeignKey(SolverOptions)
+  state = forms.CharField()
+
+class SolverOptions:
--- solver/views.py
+++ solver/views.py
@@ -1,21 +1,2 @@
 from django.shortcuts import render
 
-class Constraints:
-  creation_time = forms.DateTimeField()
-  deleted = models.BooleanField()
-  csv_table_data = forms.CharField(widget=forms.Textarea)
-
-class Schedule:
-  creation_time = forms.DateTimeField()
-  deleted = models.BooleanField()
-  score = models.IntegerField()
-  schedule = models.BinaryField()
-  created_by = models.ForeignKey(SolverRun)
-
-class SolverRun:
-  creation_time = forms.DateTimeField()
-  deleted = models.BooleanField()
-  options = models.ForeignKey(SolverOptions)
-  state = forms.CharField()
-
-class SolverOptions:
