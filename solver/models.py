da2606daae20e6005213e13c776bfa43876aa2e6
Manfred Georg <manfred.georg@gmail.com>
2014-10-08 03:58:53 +0000

    Updated model fields to first approximation.

--- solver/models.py
+++ solver/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
 class Constraints:
-  creation_time = forms.DateTimeField()
+  creation_time = models.DateTimeField()
   deleted = models.BooleanField()
-  csv_table_data = forms.CharField(widget=forms.Textarea)
+  csv_table_data = models.CharField(max_length=65535, widget=models.Textarea)
 
 class Schedule:
-  creation_time = forms.DateTimeField()
+  creation_time = models.DateTimeField()
   deleted = models.BooleanField()
   score = models.IntegerField()
-  schedule = models.BinaryField()
+  schedule = models.CharField(max_length=65535, widget=models.Textarea)
   created_by = models.ForeignKey(SolverRun)
 
 class SolverRun:
-  creation_time = forms.DateTimeField()
+  creation_time = models.DateTimeField()
   deleted = models.BooleanField()
   options = models.ForeignKey(SolverOptions)
-  state = forms.CharField()
+  state = models.ChoiceField(choices=[
+      ('i','Initialized'),
+      ('r','Running'),
+      ('n','No Solution'),
+      ('s','Solution'),
+      ('o','Optimal')])
 
 class SolverOptions:
+  arrive_late_bonus = models.IntegerField()
+  leave_early_bonus = models.IntegerField()
+  day_off_bonus = models.IntegerField()
+  preference_penalty_list = models.CommaSeparatedIntegerField(max_length=100)
+  no_break_penalty = models.CharField(max_length=1000)
+
