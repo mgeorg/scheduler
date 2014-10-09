416fd3ac57fb1b0c10f599b06afbd63d374f4805
Manfred Georg <manfred.georg@gmail.com>
2014-10-09 16:29:42 +0000

    Cleaned up template

--- solver/views.py
+++ solver/views.py
@@ -16,14 +16,11 @@ def availability(request, availability_id):
   for row in parser:
     if not row:
       continue
-    # entries = [html.escape(x.strip(), quote=True) for x in row]
     table_data.append([x.strip() for x in row])
-    # table_rows.append('<tr><td>' + '</td><td>'.join(entries) + '</td></tr>\n')
-  # table_html = '<table>\n' + ''.join(table_rows) + '</table>\n'
   template = loader.get_template('solver/availability_detail.html')
   context = RequestContext(request, {
       'id': availability_id,
       'table_data': table_data,
+      'availability': avail,
   })
   return HttpResponse(template.render(context))
-  # return HttpResponse("You're looking at availability problem %s." % availability_id)
