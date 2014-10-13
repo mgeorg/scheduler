0c2921d57f1fca73f39eee737279ba43601c4ac2
Manfred Georg <manfred.georg@gmail.com>
2014-10-13 17:39:39 +0000

    Added delete database option to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+DELETE_DATABASE=$1
+shift
+if [[ "$DELETE_DATABASE" = "--delete" ]]; then
+  echo "Deleting database"
+  sudo rm solver/migrations/0*.py
+  sudo rm db.sqlite3
+  sudo ./manage.py makemigrations
+  sudo ./manage.py migrate
+  sudo chown mgeorg:www-data db.sqlite3
+  sudo chmod 660 db.sqlite3
+fi
+
+sudo chown mgeorg:www-data -R .
+sudo chown mgeorg:www-data /tmp/django*.log
+sudo chmod 660 /tmp/django*.log
+sudo sed -i -r 's/^\s*PRODUCTION\s*=\s*FALSE\s*;?\s*$/PRODUCTION = TRUE/' scheduler/settings.py
 sudo ./manage.py collectstatic
-sudo cp solver/templates/solver/instructions.html /var/www
+sudo vi /etc/apache2/sites-enabled/000-default.conf
 sudo /etc/init.d/apache2 restart
--- solver/migrations/0001_initial.py
+++ solver/migrations/0001_initial.py
@@ -13,10 +13,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Availability',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('creation_time', models.DateTimeField(auto_now_add=True)),
                 ('deleted', models.BooleanField(default=False)),
-                ('csv_table_data', models.TextField()),
+                ('locked', models.BooleanField(default=False)),
+                ('slot_times', models.TextField()),
+                ('constraints', models.TextField()),
+                ('csv_data', models.TextField()),
             ],
             options={
             },
@@ -25,10 +28,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Schedule',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('creation_time', models.DateTimeField(auto_now_add=True)),
                 ('deleted', models.BooleanField(default=False)),
-                ('score', models.IntegerField()),
+                ('score', models.IntegerField(null=True, blank=True)),
                 ('schedule', models.TextField()),
             ],
             options={
@@ -38,11 +41,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SolverOptions',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('arrive_late_bonus', models.IntegerField()),
                 ('leave_early_bonus', models.IntegerField()),
                 ('day_off_bonus', models.IntegerField()),
-                ('no_break_penalty', models.CharField(max_length=1000)),
+                ('no_break_penalty', models.CharField(max_length=100)),
                 ('pupil_preference_penalty_list', models.CommaSeparatedIntegerField(max_length=100)),
                 ('instructor_preference_penalty_list', models.CommaSeparatedIntegerField(max_length=100)),
             ],
@@ -53,11 +56,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SolverRun',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('creation_time', models.DateTimeField(auto_now_add=True)),
                 ('solver_version', models.CharField(max_length=10)),
                 ('deleted', models.BooleanField(default=False)),
-                ('score', models.IntegerField(null=True)),
+                ('score', models.IntegerField(null=True, blank=True)),
                 ('scheduler_output', models.TextField()),
                 ('solver_output', models.TextField()),
                 ('state', models.CharField(max_length=1, choices=[('i', 'Solver Initialized'), ('r', 'Solver Running'), ('d', 'Solver Done')])),
